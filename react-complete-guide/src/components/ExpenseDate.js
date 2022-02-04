f20b3df2e4ccfc0d92a1e6fd3f6364fa46ab3be4
Connor Roberts <connoobets@gmail.com>
2022-02-04 14:01:47 -0700

    add ExpenseDate component and css file as well as add eslintcache to git ignore, refactor ExpenseItem component to destructure props and exclude date related constants

---
+++ react-complete-guide/src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+function ExpenseDate(props) {
+
+  // extract each piece of date from props
+  const month = props.date.toLocaleString("en-US", { month: 'long' });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: '2-digit' });
+
+  return (
+    <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+  )
+}
+
+export default ExpenseDate;
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+
+
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: 'long' });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("en-US", { day: '2-digit' });
+
+  // destructure props
+  const { date, title, amount } = props;
+
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
