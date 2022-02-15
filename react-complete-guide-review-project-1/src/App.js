a07340c80e3da2ab624e41b95d65dc23b8adbf23
Connor Roberts <connoobets@gmail.com>
2022-02-15 14:59:18 -0700

    add Card and User components to App component and add styles to card component

--- react-complete-guide-review-project-1/src/App.js
+++ react-complete-guide-review-project-1/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Card from './Components/Card';
+import User from './Components/User';
 
 
 function App() {
   return (
     <div>
+      <Card>
+        <User />
+      </Card>
 
     </div>
   );
--- react-complete-guide-review-project-1/src/Components/User.js
+++ react-complete-guide-review-project-1/src/Components/User.js
@@ -3,6 +3,17 @@ import styles from './User.module.css';
 
 const User = props => {
 
+  return (
+    <div>
+      <form>
+        <label>Username</label>
+        <input />
+        <label>Age (Years)</label>
+        <input />
+        <button type='submit'>Add User</button>
+      </form>
+    </div>
+  )
 };
 
 export default User;
