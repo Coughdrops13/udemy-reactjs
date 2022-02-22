1665db55f5d4ffe571bc312ec52f2630cd870d87
Connor Roberts <connoobets@gmail.com>
2022-02-22 11:44:17 -0700

    create MealItemForm component with css file and add it to AvailableMeals component

---
+++ react-complete-guide-food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+  return (
+    <form>
+
+    </form>
+  )
+};
+
+export default MealItemForm;
