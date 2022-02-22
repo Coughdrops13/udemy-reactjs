7acc8e365afe8bffa000ce786e36c1286a6f5f36
Connor Roberts <connoobets@gmail.com>
2022-02-22 10:45:43 -0700

    add CartIcon component to cart folder

--- react-complete-guide-food-order-app/src/components/Layout/HeaderCartButton.js
+++ react-complete-guide-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const HeaderCartButton = (props) => {
-
+  return (
+    <button>
+      <span>
+        
+      </span>
+      <span></span>
+      <span></span> 
+    </button>
+  )
 };
 
 export default HeaderCartButton;
