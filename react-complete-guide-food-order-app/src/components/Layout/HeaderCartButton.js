3ee51e0211f73ab737776c7bf6c8d2f6765e35ab
Connor Roberts <connoobets@gmail.com>
2022-02-22 10:51:50 -0700

    create HeaderCartButton component with class modules css file and add it to Header component

--- react-complete-guide-food-order-app/src/components/Layout/HeaderCartButton.js
+++ react-complete-guide-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   return (
-    <button>
-      <span>
-        
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
       </span>
-      <span></span>
-      <span></span> 
+      <span>Your Cart</span>
+      <span className={classes.badge}>Item Count</span>
     </button>
-  )
+  );
 };
 
 export default HeaderCartButton;
