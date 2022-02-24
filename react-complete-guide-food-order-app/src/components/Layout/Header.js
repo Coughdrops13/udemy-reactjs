ecd991613f27ac51fdee6d012413730fcecdc871
Connor Roberts <connoobets@gmail.com>
2022-02-24 10:13:36 -0700

    add showcart and hidecart handlers using prop drilling to close button on Cart component and backdrop of Modal component

--- react-complete-guide-food-order-app/src/components/Layout/Header.js
+++ react-complete-guide-food-order-app/src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>Connor's Comestibles</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onShowCart={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of charcuterie!"/>
