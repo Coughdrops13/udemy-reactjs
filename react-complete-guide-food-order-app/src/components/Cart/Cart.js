fbcc2f237b33baadfd425f00ed08083d24627da5
Connor Roberts <connoobets@gmail.com>
2022-02-24 11:33:35 -0700

    add logic to output proper total in Cart component using useContext. Also Order button dynamically appears based on if cart has items in it

--- react-complete-guide-food-order-app/src/components/Cart/Cart.js
+++ react-complete-guide-food-order-app/src/components/Cart/Cart.js
@@ -7,8 +7,11 @@ import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
@@ -19,11 +22,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>$35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
