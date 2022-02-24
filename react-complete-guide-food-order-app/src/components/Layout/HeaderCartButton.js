227a22c67d604a942209055e540d4bea3c61e8f4
Connor Roberts <connoobets@gmail.com>
2022-02-24 10:33:37 -0700

    use cert context in HeaderCartButton component

--- react-complete-guide-food-order-app/src/components/Layout/HeaderCartButton.js
+++ react-complete-guide-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
   return (
     <button className={classes.button} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>Item Count</span>
+      <span className={classes.badge}>{cartCtx.items.length}</span>
     </button>
   );
 };
