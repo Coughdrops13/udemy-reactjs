510ac100aa9cd0cd701273074698dc9faa37140a
Connor Roberts <connoobets@gmail.com>
2022-02-24 10:38:23 -0700

    use reducer() function to total amount of items in cartCtx.items array

--- react-complete-guide-food-order-app/src/components/Layout/HeaderCartButton.js
+++ react-complete-guide-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -7,13 +7,17 @@ import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
 
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0)
+
   return (
     <button className={classes.button} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{cartCtx.items.length}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
