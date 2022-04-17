4b40b77f50288d9511bb2ead36e6afcbef78cc10
Connor Roberts <connoobets@gmail.com>
2022-04-17 10:24:43 -0600

    create Header component. assets folder in src folder with meals.jpg for use by Header component

---
+++ react-complete-guide-food-order-app-REDO/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg';
+
+const Header = (props) => {
+  return (
+    <>
+      <header>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img src={mealsImage} alt="A table of delicious meats" />
+      </div>
+    </>
+  )
+};
+
+export default Header;
