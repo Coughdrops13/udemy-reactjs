898d3c262d806b735d109975dbb81f4c0833779f
Connor Roberts <connoobets@gmail.com>
2022-04-19 09:26:31 -0600

    create AvailableMeals component with map() to output list items for each dummy meal

--- react-complete-guide-food-order-app-REDO/src/App.js
+++ react-complete-guide-food-order-app-REDO/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from './components/Layout/Header';
+import AvailableMeals from './components/Meals/AvailableMeals';
 
 function App() {
   return (
