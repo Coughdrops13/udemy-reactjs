65c661bc3fb053839f657a7e266ac0c0c0661598
Connor Roberts <connoobets@gmail.com>
2022-04-19 10:41:42 -0600

    create MealItem component with modules and MealItemForm file. edit AvailableMeals component to render multiple MealItem components instead of list items

---
+++ react-complete-guide-food-order-app-REDO/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from 'MealItemForm.module.css';
+
+const MealItemForm = () => {
+  return (
+    <form className={classes.form}>
+    </form>
+  );
+};
+
+export default MealItemForm;
