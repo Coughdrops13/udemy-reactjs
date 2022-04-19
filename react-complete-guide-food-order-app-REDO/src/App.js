683019a8db1a0b9787f46e1cce5cd593419bd355
Connor Roberts <connoobets@gmail.com>
2022-04-19 10:30:42 -0600

    add Card component, MealItem folder, MealItem component and MealItem.module.css

--- react-complete-guide-food-order-app-REDO/src/App.js
+++ react-complete-guide-food-order-app-REDO/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
+import Meals from './components/Meals/Meals';import Card from './components/UI/Card';
 
 function App() {
   return (
--- react-complete-guide-food-order-app-REDO/src/components/Meals/AvailableMeals.js
+++ react-complete-guide-food-order-app-REDO/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -29,11 +30,13 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
