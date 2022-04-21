a8e4fa73f29f9c63f48d90e1868a02a70b691c98
Connor Roberts <connoobets@gmail.com>
2022-04-21 10:15:34 -0600

    formatting corrections in Input component

--- react-complete-guide-food-order-app-REDO/src/components/UI/Input.js
+++ react-complete-guide-food-order-app-REDO/src/components/UI/Input.js
@@ -5,9 +5,9 @@ const Input = (props) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input  {...props.input}/>
+      <input  {...props.input} />
     </div>
-  )
+  );
 };
 
 export default Input;
