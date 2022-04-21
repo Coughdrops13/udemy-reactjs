a5e4b0a60ae64e5adc1e1aeb7b3da1050341ecce
Connor Roberts <connoobets@gmail.com>
2022-04-21 10:47:51 -0600

    add state for showing and hiding modal and create functions to handle clicks

--- react-complete-guide-food-order-app-REDO/src/App.js
+++ react-complete-guide-food-order-app-REDO/src/App.js
@@ -1,13 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }; 
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
