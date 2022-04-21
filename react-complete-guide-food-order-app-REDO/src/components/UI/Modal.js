95d0d9b650f09aa98b7cdb9bc6c788655d410b21
Connor Roberts <connoobets@gmail.com>
2022-04-21 10:24:09 -0600

    create Backdrop and ModalOveraly components within Modal component file with portal to div with id=overlays

--- react-complete-guide-food-order-app-REDO/src/components/UI/Modal.js
+++ react-complete-guide-food-order-app-REDO/src/components/UI/Modal.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import classes from './Modal.module.css';
+import React, { Fragment } from "react";
+import reactDom from "react-dom";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOveraly = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children} </div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
-    <div>
-
-    </div>
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {reactDom.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
   );
 };
 
