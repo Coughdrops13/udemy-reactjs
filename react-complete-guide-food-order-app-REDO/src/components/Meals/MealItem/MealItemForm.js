4472e7f7c83b93547da7f1750c3571c48b7075ad
Connor Roberts <connoobets@gmail.com>
2022-04-21 09:53:04 -0600

    complete imports

--- react-complete-guide-food-order-app-REDO/src/components/Meals/MealItem/MealItemForm.js
+++ react-complete-guide-food-order-app-REDO/src/components/Meals/MealItem/MealItemForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Input from "../../UI/Input";
-import classes from "MealItemForm.module.css";
+import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
   return (
