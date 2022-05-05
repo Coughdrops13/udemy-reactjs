0fb8199acd3681e2301a7cd8e9b9379690e3235d
Connor Roberts <connoobets@gmail.com>
2022-05-04 20:41:28 -0600

    add schwarzmullers code additions

--- react-complete-guide-star-wars-api/src/App.js
+++ react-complete-guide-star-wars-api/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
-
+import AddMovie from './components/AddMovie';
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -44,6 +44,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>No movies found.</p>;
 
   if (!isLoading && movies.length > 0) {
@@ -59,6 +63,9 @@ function App() {
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
@@ -67,3 +74,80 @@ function App() {
 }
 
 export default App;
+
+
+// the following section is Maximillian Schwarzmuller's code
+
+// import React, { useState, useEffect, useCallback } from 'react';
+
+// import MoviesList from './components/MoviesList';
+// import AddMovie from './components/AddMovie';
+// import './App.css';
+
+// function App() {
+//   const [movies, setMovies] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   const fetchMoviesHandler = useCallback(async () => {
+//     setIsLoading(true);
+//     setError(null);
+//     try {
+//       const response = await fetch('https://swapi.dev/api/films/');
+//       if (!response.ok) {
+//         throw new Error('Something went wrong!');
+//       }
+
+//       const data = await response.json();
+
+//       const transformedMovies = data.results.map((movieData) => {
+//         return {
+//           id: movieData.episode_id,
+//           title: movieData.title,
+//           openingText: movieData.opening_crawl,
+//           releaseDate: movieData.release_date,
+//         };
+//       });
+//       setMovies(transformedMovies);
+//     } catch (error) {
+//       setError(error.message);
+//     }
+//     setIsLoading(false);
+//   }, []);
+
+//   useEffect(() => {
+//     fetchMoviesHandler();
+//   }, [fetchMoviesHandler]);
+
+//   function addMovieHandler(movie) {
+//     console.log(movie);
+//   }
+
+//   let content = <p>Found no movies.</p>;
+
+//   if (movies.length > 0) {
+//     content = <MoviesList movies={movies} />;
+//   }
+
+//   if (error) {
+//     content = <p>{error}</p>;
+//   }
+
+//   if (isLoading) {
+//     content = <p>Loading...</p>;
+//   }
+
+//   return (
+//     <React.Fragment>
+//       <section>
+//         <AddMovie onAddMovie={addMovieHandler} />
+//       </section>
+//       <section>
+//         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+//       </section>
+//       <section>{content}</section>
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
