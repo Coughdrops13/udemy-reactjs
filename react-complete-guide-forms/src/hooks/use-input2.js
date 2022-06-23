45fb4161720a7d4d8f5ba99338ec3ac5ce672f1d
Connor Roberts <connoobets@gmail.com>
2022-06-23 10:31:03 -0600

    implement useReducer in custom hook use-input and delete unused component InputField

--- react-complete-guide-forms/src/hooks/use-input2.js
+++ react-complete-guide-forms/src/hooks/use-input2.js
@@ -1,26 +1,59 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return {
+      value: action.value,
+      isTouched: state.isTouched
+    };
+  }
+
+  if (action.type === "BLUR") {
+    return {
+      value: state.value,
+      isTouched: true,
+    }
+  }
+
+  if (action.type === "RESET") {
+    return initialInputState;
+  }
+
+  return inputStateReducer;
+};
 
 const useInput2 = (validateValue) => {
-  const [enteredValue2, setEnteredValue2] = useState("");
-  const [isTouched2, setIsTouched2] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const valueIsValid2 = validateValue(enteredValue2);
-  const isError2 = !valueIsValid2 && isTouched2;
+  const valueIsValid2 = validateValue(inputState.value);
+  const isError2 = !valueIsValid2 && inputState.isTouched;
 
   const valueChangeHandler2 = (event) => {
-    setEnteredValue2(event.target.value);
+    dispatch({
+      type: "INPUT",
+      value: event.target.value,
+    });
   };
 
   const inputBlurHandler2 = (event) => {
-    setIsTouched2(true);
+    dispatch({
+      type: "BLUR"
+    });
   };
 
   const reset2 = (event) => {
-    setIsTouched2(false);
-    setEnteredValue2("");
+    dispatch({type: "RESET"})
   };
   return {
-    value2: enteredValue2,
+    value2: inputState.value,
     isValid2: valueIsValid2,
     isError2,
     valueChangeHandler2,
