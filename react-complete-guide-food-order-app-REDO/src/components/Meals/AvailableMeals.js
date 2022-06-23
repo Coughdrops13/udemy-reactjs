c349bda1c1539b0ee9a2a11b27abaa4a9c30bc9c
Connor Roberts <connoobets@gmail.com>
2022-06-23 11:40:03 -0600

    attempt at fetch available meals from firebase

--- react-complete-guide-food-order-app-REDO/src/components/Meals/AvailableMeals.js
+++ react-complete-guide-food-order-app-REDO/src/components/Meals/AvailableMeals.js
@@ -3,35 +3,38 @@ import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  let loadedMeals = [];
+  fetch("https://react-http-95836-default-rtdb.firebaseio.com/meals.json")
+  .catch((err) => {
+    console.log("ERROR", err.message);
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    
+    for (const key in data) {
+      loadedMeals.push({
+        id: key,
+        name: data[key].name,
+        description: data[key].description,
+        price: data[key].price,
+      })
+    }
+    console.log('LOADEDMEALS:', loadedMeals);
+
+    return loadedMeals;
+    
+  })
+  .catch((err) => {
+    console.log("ERROR", err.message);
+  });
+  
+  console.log("LOADEDMEALS 2:", loadedMeals);
+
+  const mealsList = loadedMeals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -40,6 +43,8 @@ const AvailableMeals = () => {
       price={meal.price}
     />
   ));
+    
+    
 
   return (
     <section className={classes.meals}>
