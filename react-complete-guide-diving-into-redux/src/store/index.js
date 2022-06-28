4a1cae0c0779a7543ae38f70e15415b5618afc6e
Connor Roberts <connoobets@gmail.com>
2022-06-28 17:07:01 -0600

    add forgotten pieces to counterReducer redux logic

--- react-complete-guide-diving-into-redux/src/store/index.js
+++ react-complete-guide-diving-into-redux/src/store/index.js
@@ -1,12 +1,20 @@
-const redux = require('redux');
+import redux from "redux";
 
-const reducer = (state, action) => {
-  return {
-    
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
   }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    }
+  }
+
+  return state;
 };
-const store = redux.createStore(reducer);
 
+const store = redux.createStore(counterReducer);
 
-
-store.dispatch
+store.dispatch;
