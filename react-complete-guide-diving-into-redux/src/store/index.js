f2ca85e191eea2674547e3954d28a11987f50a48
Connor Roberts <connoobets@gmail.com>
2022-06-29 16:12:34 -0600

    reinstall redux

--- react-complete-guide-diving-into-redux/src/store/index.js
+++ react-complete-guide-diving-into-redux/src/store/index.js
@@ -1,11 +1,11 @@
-import { createSlice, createStore } from "reduxjs/toolkit";
+import { createSlice, createStore } from "@reduxjs/toolkit";
 
 const initialState = {
   counter: 0,
   showCounter: true,
 };
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
@@ -24,33 +24,33 @@ createSlice({
   }
 })
 
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "toggle":
-      return {
-        ...state,
-        showCounter: !state.showCounter,
-      }
-    case 'increment':
-      return {
-        ...state,
-        counter: state.counter + 1,
-      }
-    case 'decrement':
-      return {
-        ...state,
-        counter: state.counter - 1,
-      }
-    case 'increase':
-      return {
-        ...state,
-        counter: state.counter + action.amount,
-      }
-    default:
-      return initialState;
-    }
-};
+// const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "toggle":
+//       return {
+//         ...state,
+//         showCounter: !state.showCounter,
+//       }
+//     case 'increment':
+//       return {
+//         ...state,
+//         counter: state.counter + 1,
+//       }
+//     case 'decrement':
+//       return {
+//         ...state,
+//         counter: state.counter - 1,
+//       }
+//     case 'increase':
+//       return {
+//         ...state,
+//         counter: state.counter + action.amount,
+//       }
+//     default:
+//       return initialState;
+//     }
+// };
 
-const store = createStore(counterReducer);
+const store = createStore(counterSlice.reducer);
 
 export default store;
