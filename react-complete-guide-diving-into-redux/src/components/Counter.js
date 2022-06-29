9e228ca0071c6d95b050b7187d6c0423fbaecfc5
Connor Roberts <connoobets@gmail.com>
2022-06-29 14:36:23 -0600

    dispatch commands from Counter component using redux

--- react-complete-guide-diving-into-redux/src/components/Counter.js
+++ react-complete-guide-diving-into-redux/src/components/Counter.js
@@ -1,17 +1,48 @@
-import classes from './Counter.module.css';
-import { useSelector } from 'react-redux';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { Component } from 'react';
 
 const Counter = () => {
-  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
+
+class classCounter extends Component {
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{counter}</div>
+        <div>
+          <button onClick={incrementHandler}>Increment</button>
+          <button onClick={decrementHandler}>Decrement</button>
+        </div>
+        <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+};
+
+
 
 export default Counter;
