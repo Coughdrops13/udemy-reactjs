eae3a8dc0f58783da9bc326b6679535b5402dbe5
Connor Roberts <connoobets@gmail.com>
2022-06-30 09:17:53 -0600

    split up index.js file in store folder so that each store slice has its own file

--- react-complete-guide-diving-into-redux/src/store/index.js
+++ react-complete-guide-diving-into-redux/src/store/index.js
@@ -1,46 +1,7 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialCounterState = {
-  counter: 0,
-  showCounter: true,
-};
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--
-    },
-    increase(state, action) {
-      state.counter += action.payload;
-    },
-  }
-})
-
-const initialAuthState = {
-  isAuthenticated: false
-};
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  }
-})
-
+import counterSlice from "./counter-slice";
+import authSlice from "./auth-slice";
 
 // with multiple slices, there can be multiple reducers given to the configureSlice method
 const store = configureStore({
@@ -55,6 +16,8 @@ export const authActions = authSlice.actions
 
 export default store;
 
+
+// reducer with redux and without reduxjs/toolkit
 // const counterReducer = (state = initialCounterState, action) => {
 //   switch (action.type) {
 //     case "toggle":
