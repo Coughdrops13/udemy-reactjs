38bdba8012803cca21740c0a4851cdcf751660a0
Connor Roberts <connoobets@gmail.com>
2022-06-30 12:31:57 -0600

    create addItemToCart function in cartReducer

--- react-complete-guide-advanced-redux/src/store/cart-slice.js
+++ react-complete-guide-advanced-redux/src/store/cart-slice.js
@@ -1,14 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialCartState = {
-
+  items: [],
+  totalQuantity: 0,
+  totalAmount: 0,
 };
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: initialCartState,
   reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          title: newItem.title,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price
+        });
+      }
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
 
+    },
   }
 });
 
