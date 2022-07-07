7abe84abaf9311f17f27237df701d88edd1f16ef
Connor Roberts <connoobets@gmail.com>
2022-07-07 10:35:32 -0600

    use useSelector to get ui.notification status, title, and message and pass those props to Notification component that is conditionally rendered if notification is truthy

--- react-complete-guide-advanced-redux/src/App.js
+++ react-complete-guide-advanced-redux/src/App.js
@@ -1,17 +1,19 @@
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { uIActions } from "./store/ui-slice";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
-
 
   useEffect(() => {
     const sendCartData = async () => {
@@ -37,6 +39,11 @@ function App() {
 
     };
 
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
     sendCartData().catch(error => {
       dispatch(uIActions.showNotification({
         status: "error",
@@ -47,11 +54,17 @@ function App() {
     
   }, [cart, dispatch]);
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
 export default App;
+
+
+
