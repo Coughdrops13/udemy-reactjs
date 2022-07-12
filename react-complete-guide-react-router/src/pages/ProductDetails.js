075b4d13742697348c6aa381b21b756dfd76dd58
Connor Roberts <connoobets@gmail.com>
2022-07-12 14:17:54 -0600

    use useParams hook from react-router-dom to get product Id from url and use it in ProductDetails component

--- react-complete-guide-react-router/src/pages/ProductDetails.js
+++ react-complete-guide-react-router/src/pages/ProductDetails.js
@@ -1,9 +1,12 @@
-import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
+  const params = useParams();
+
   return (
     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   )
 };
