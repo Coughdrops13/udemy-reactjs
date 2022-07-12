955ec77978692c171156b931cd5879010cfa60df
Connor Roberts <connoobets@gmail.com>
2022-07-12 13:43:34 -0600

    give activeClassName to NavLink components for better ux

--- react-complete-guide-react-router/src/components/MainHeader.js
+++ react-complete-guide-react-router/src/components/MainHeader.js
@@ -8,10 +8,10 @@ const MainHeader = () => {
       <nav>
         <ul>
           <li>
-            <NavLink to="/welcome">Welcome</NavLink>
+            <NavLink to="/welcome" activeClassName={classes.active}>Welcome</NavLink>
           </li>
           <li>
-            <NavLink to="/products">Products</NavLink>
+            <NavLink to="/products" activeClassName={classes.active}>Products</NavLink>
           </li>
         </ul>
       </nav>
