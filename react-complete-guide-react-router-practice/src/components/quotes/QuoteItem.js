0be6f103abf0c7c6eb3140742c71dbbbaf85bc57
Connor Roberts <connoobets@gmail.com>
2022-07-12 15:33:59 -0600

    my attempt at adding nested Routes for comments on individual quotes

--- react-complete-guide-react-router-practice/src/components/quotes/QuoteItem.js
+++ react-complete-guide-react-router-practice/src/components/quotes/QuoteItem.js
@@ -1,13 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Route } from 'react-router-dom';
+import { Fragment } from 'react';
 
 
 import classes from './QuoteItem.module.css';
+import CommentsList from '../comments/CommentsList';
+import NewCommentForm from '../comments/NewCommentForm';
 
 const QuoteItem = (props) => {
   const params = useParams();
 
   return (
-    <h1>{params.quoteId}</h1>
+    <Fragment>
+      <h1>Quote Details</h1>
+      <p>{params.quoteId}</p>
+      <Route path='/quotes/:quoteId/comments'>
+        <CommentsList />
+        <NewCommentForm />
+      </Route>
+    </Fragment>
   )
 };
 
