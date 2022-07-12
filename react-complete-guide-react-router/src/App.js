633de0be617a0374f1449b79485d27720af93532
Connor Roberts <connoobets@gmail.com>
2022-07-12 11:20:52 -0600

    project setup for react-router module

---
+++ react-complete-guide-react-router/src/App.js
@@ -0,0 +1,16 @@
+import { Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
+
+
+// ourDomain.com/ => Component A
+// outDomain.com/products => Component B
