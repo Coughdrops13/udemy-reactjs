50471071c9646be352e1772f63d60f5a8f891953
Connor Roberts <connoobets@gmail.com>
2022-07-12 13:47:56 -0600

    create ProductDetail page/component

---
+++ react-complete-guide-react-router/src/pages/ProductDetails.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const ProductDetail = () => {
+  return (
+    <Fragment>
+
+    </Fragment>
+  )
+};
+
+export default ProductDetail;
