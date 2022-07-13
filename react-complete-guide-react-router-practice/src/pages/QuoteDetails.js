baec3c1876003a914fe26f3f6a01893070c37404
Connor Roberts <connoobets@gmail.com>
2022-07-13 09:02:13 -0600

    import HighlightedQuote component to QuoteDetails component and use it to display quotes when clicked on AllQuotes page

--- react-complete-guide-react-router-practice/src/pages/QuoteDetails.js
+++ react-complete-guide-react-router-practice/src/pages/QuoteDetails.js
@@ -1,18 +1,42 @@
 import { useParams, Route } from 'react-router-dom';
 import { Fragment } from 'react';
 
-
-// import classes from './QuoteItem.module.css';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Bat Man",
+    text: `WHERE'S RACHEL?`,
+  },
+  {
+    id: "q2",
+    author: "Bat Man",
+    text: "Alfred.",
+  },
+  {
+    id: 'q3',
+    author: "Bat Man",
+    text: "*grunts",
+  }
+];
 
 
 const QuoteDetails = (props) => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+  if (!quote) {
+    return (
+      <p>No quote found</p>
+    )
+  }
+
   return (
     <Fragment>
-      <h1>Quote Details</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
